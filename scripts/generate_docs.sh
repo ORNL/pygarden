58ab71479d78f109c6690769c18bd379abf57d84
Kyle Medlen <w2v@ornl.gov>
2025-07-25 16:46:13 -0400

    rename script

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -5,6 +5,6 @@ set -e  # fail immediately on error
 uv sync
 source .venv/bin/activate
 # run the auto documentation generator
-python ./scripts/gen_api_docs.py
+python ./scripts/gen_pygarden_docs.py
 # build the documentation (clean slate)
 mkdocs build --clean
