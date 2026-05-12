de24f63bb16b431e2dfbfea3d1e0679b89dc537e
Kyle Medlen <w2v@ornl.gov>
2026-05-12 11:26:38 -0400

    respond to mr comments/add unit test

--- src/pygarden/mixins/postgres_logger.py
+++ src/pygarden/mixins/postgres_logger.py
@@ -42,6 +42,7 @@ class PostgresLoggerMixin(Database, PostgresMixin):
             self.log_to_database("DEBUG", message)
         elif c:
             self.collect_logs("DEBUG", message)
+
         self.logger.debug(message)
 
     def info(self, message, w=False, c=False):
@@ -51,6 +52,7 @@ class PostgresLoggerMixin(Database, PostgresMixin):
             self.log_to_database("INFO", message)
         elif c:
             self.collect_logs("INFO", message)
+
         self.logger.info(message)
 
     def warning(self, message, w=False, c=False):
@@ -60,6 +62,7 @@ class PostgresLoggerMixin(Database, PostgresMixin):
             self.log_to_database("WARNING", message)
         elif c:
             self.collect_logs("WARNING", message)
+
         self.logger.warning(message)
 
     def error(self, message, w=False, c=False):
@@ -69,63 +72,69 @@ class PostgresLoggerMixin(Database, PostgresMixin):
             self.log_to_database("ERROR", message)
         elif c:
             self.collect_logs("ERROR", message)
+
         self.logger.error(message)
 
     def critical(self, message, w=False, c=False):
-        """Logs a critical Message"""
+        """Logs a critical message."""
         if w:
             self.check_table_exists()
             self.log_to_database("CRITICAL", message)
         elif c:
             self.collect_logs("CRITICAL", message)
+
         self.logger.critical(message)
 
     # Only call from an exception handler
     def exception(self, message, w=False, c=False):
-        """Logs and exception message."""
+        """Logs an exception message."""
         if w:
             self.check_table_exists()
             self.log_to_database("EXCEPTION", message)
         elif c:
             self.collect_logs("EXCEPTION", message)
+
         self.logger.exception(message)
 
     def check_table_exists(self):
         """Checks if log table exists for workspace."""
         self.open()
+
         self.cursor.execute(
             f"""
             CREATE TABLE IF NOT EXISTS {self.schema}.{self.table_name} (
                 levelname TEXT,
                 message TEXT,
-                ts timestamp default now()
+                ts TIMESTAMPTZ DEFAULT NOW()
             );
             """
         )
+
         self.close()
 
     def log_to_database(self, loglevel, message):
         """Logs log message to database."""
         self.open()
+
         self.cursor.execute(
             f"""
             INSERT INTO {self.schema}.{self.table_name} (levelname, message)
-            VALUES('{loglevel}', '{message}');
-            """
+            VALUES (%s, %s);
+            """,
+            (loglevel, message),
         )
+
         self.close()
 
     def collect_logs(self, level, message):
-        """Appends log messages to list"""
+        """Appends log messages to list."""
         log = (level, message)
         self.log_collection.append(tuple(log))
 
     def write_log_collection_to_database(self, log_list=None):
-        """Writes collection of logs to database and empties log list"""
+        """Writes collection of logs to database and empties log list."""
         self.check_table_exists()
 
-        # This keeps us from mucking up the internal list, even though we double
-        # the code
         self.open()
 
         if log_list:
@@ -133,8 +142,10 @@ class PostgresLoggerMixin(Database, PostgresMixin):
                 self.cursor.execute(
                     f"""
                     INSERT INTO {self.schema}.{self.table_name}
-                    VALUES('{log_entry[0]}', '{log_entry[1]}');
-                    """
+                    (levelname, message)
+                    VALUES (%s, %s);
+                    """,
+                    (log_entry[0], log_entry[1]),
                 )
 
         elif self.log_collection:
@@ -142,9 +153,12 @@ class PostgresLoggerMixin(Database, PostgresMixin):
                 self.cursor.execute(
                     f"""
                     INSERT INTO {self.schema}.{self.table_name}
-                    VALUES('{log_entry[0]}', '{log_entry[1]}');
-                    """
+                    (levelname, message)
+                    VALUES (%s, %s);
+                    """,
+                    (log_entry[0], log_entry[1]),
                 )
+
             self.log_collection = []
 
         else:
